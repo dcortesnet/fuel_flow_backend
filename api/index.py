808e215650e8cf075f61c1daf69a60258fe35d59
diego <dcortes.net@gmail.com>
2025-11-07 20:52:42 -0300

    test

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from routes import pedidos_bp, auth_bp
-from config import Config
+#from routes import pedidos_bp, auth_bp
+#from config import Config
 
 app = Flask(__name__)
 #app.config.from_object(Config)
